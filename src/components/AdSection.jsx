4bb001950c596191a575da9e9245be969bf43e1b
Iman1609 <imanfatima1609@gmail.com>
2025-03-07 02:45:22 -0800

    Update AdSection.jsx

--- src/components/AdSection.jsx
+++ src/components/AdSection.jsx
@@ -6,7 +6,7 @@ function AdSection() {
   return (
     <div className="relative flex justify-center items-center w-auto mx-auto md:mx-auto mt-4">
       <img src={adBanner} alt="Mid Month Sale Banner" className="h-auto w-full rounded-full object-cover" />
-      <button className="absolute h-16 w-16 md:h-24 md:w-24 bg-yellow-300 text-pink-950 font-bold rounded-full right-2 md:right-20 animate-pulse">
+      <button className="absolute h-10 w-10 md:h-16 md:w-16 bg-yellow-300 text-pink-950 font-bold rounded-full right-2 md:right-20 animate-pulse">
         Shop Now
       </button>
     </div>
